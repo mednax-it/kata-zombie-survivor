aae861e4f142b93f15521f0e7b020609d7eb82e8
Kyle Adams <kadams54@users.noreply.github.com>
2022-05-24 09:57:21 -0400

    Create a new Survivor

---
+++ python/test/test_zombie_survivor.py
@@ -0,0 +1,11 @@
+from zombie_survivor.survivor import Survivor
+
+class TestSurvivor:
+    def setup(self):
+        self.survivor = Survivor('Gandalf')
+
+    def test_creating_a_new_survivor(self):
+        assert self.survivor.name == 'Gandalf'
+        assert self.survivor.wounds == 0
+        assert self.survivor.remaining_actions == 3
+        assert self.survivor.is_alive
