03ae99e03e2127e79669edc75add76c0902002f7
Kyle Adams <kadams54@users.noreply.github.com>
2022-07-26 10:04:11 -0400

    Black formatting updates

--- python/zombie_survivor/survivor.py
+++ python/zombie_survivor/survivor.py
@@ -1,16 +1,16 @@
 class Survivor:
-  def __init__(self, name: str):
-    self.name = name
-    self.wounds = 0
-    self._actions_remaining = 3
-  
-  def is_alive(self) -> bool:
-    return self.wounds < 2
-  
-  @property
-  def actions_remaining(self) -> int:
-    return self._actions_remaining
-  
-  @actions_remaining.setter
-  def actions_remaining(self, num: int):
-    self._actions_remaining = num
+    def __init__(self, name: str):
+        self.name = name
+        self.wounds = 0
+        self._actions_remaining = 3
+
+    def is_alive(self) -> bool:
+        return self.wounds < 2
+
+    @property
+    def actions_remaining(self) -> int:
+        return self._actions_remaining
+
+    @actions_remaining.setter
+    def actions_remaining(self, num: int):
+        self._actions_remaining = num
