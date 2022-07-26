7a742bb45e95d3d37d4d0594b27d0be0ea985319
Kyle Adams <kadams54@users.noreply.github.com>
2022-07-26 10:04:11 -0400

    Black formatting updates

--- python/test/test_survivor.py
+++ python/test/test_survivor.py
@@ -1,15 +1,16 @@
 from zombie_survivor.survivor import Survivor
 
+
 class TestSurvivor:
-  def setup(self):
-    self.survivor = Survivor('Rob Zombie')
-  
-  def test_initial_setup(self):
-    assert self.survivor.name == 'Rob Zombie'
-    assert self.survivor.wounds == 0
-    assert self.survivor.is_alive()
-    assert self.survivor.actions_remaining == 3
-  
-  def test_setter(self):
-    self.survivor.actions_remaining = 5
-    assert self.survivor.actions_remaining == 5
+    def setup(self):
+        self.survivor = Survivor("Rob Zombie")
+
+    def test_initial_setup(self):
+        assert self.survivor.name == "Rob Zombie"
+        assert self.survivor.wounds == 0
+        assert self.survivor.is_alive()
+        assert self.survivor.actions_remaining == 3
+
+    def test_setter(self):
+        self.survivor.actions_remaining = 5
+        assert self.survivor.actions_remaining == 5
