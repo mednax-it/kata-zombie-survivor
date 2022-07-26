6bea8a0e820e378f67b8a063c44427715cee5be9
Kyle Adams <kadams54@users.noreply.github.com>
2022-07-26 10:51:06 -0400

    Initial pass at implementing equipment storage

--- python/test/test_survivor.py
+++ python/test/test_survivor.py
@@ -12,7 +12,16 @@ class TestSurvivor:
         assert self.survivor.wounds == 0
         assert self.survivor.is_alive()
         assert self.survivor.actions_remaining == 3
+        assert self.survivor.equipment_in_hand == ["Baseball bat", "Frying pan"]
+        assert self.survivor.equipment_in_reserve == [
+            "Bottled water",
+            "Katana",
+            "Pistol",
+        ]
 
     def test_setter(self):
         self.survivor.actions_remaining = 5
         assert self.survivor.actions_remaining == 5
+
+    def test_add_equipment(self):
+        pass
