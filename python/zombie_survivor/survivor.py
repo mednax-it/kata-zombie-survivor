ad036eb18e76a437d1c14f8eba9b25d724efe244
Kyle Adams <kadams54@users.noreply.github.com>
2022-08-11 10:50:17 -0400

    Allow survivor to pick up items

--- python/zombie_survivor/survivor.py
+++ python/zombie_survivor/survivor.py
@@ -1,8 +1,11 @@
+INITIAL_EQUIPMENT_LIMIT = 5
+
 class Survivor:
     def __init__(self, name: str):
         self.name = name
         self.wounds = 0
         self._actions_remaining = 3
+        self._equipment = []
 
     def is_alive(self) -> bool:
         return self.wounds < 2
@@ -14,3 +17,10 @@ class Survivor:
     @actions_remaining.setter
     def actions_remaining(self, num: int):
         self._actions_remaining = num
+
+    @property
+    def space_remaining(self) -> int:
+        return INITIAL_EQUIPMENT_LIMIT - len(self._equipment)
+
+    def pick_up(self, item: str):
+        self._equipment.append(item)
