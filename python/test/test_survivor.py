154e43aa99e5d67ca6db1626d900c74c0abfc067
Kyle Adams <kadams54@users.noreply.github.com>
2022-08-11 10:50:17 -0400

    Allow survivor to pick up items

--- python/test/test_survivor.py
+++ python/test/test_survivor.py
@@ -1,4 +1,4 @@
-from zombie_survivor.survivor import Survivor
+from zombie_survivor.survivor import Survivor, INITIAL_EQUIPMENT_LIMIT
 
 
 class TestSurvivor:
@@ -12,7 +12,13 @@ class TestSurvivor:
         assert self.survivor.wounds == 0
         assert self.survivor.is_alive()
         assert self.survivor.actions_remaining == 3
+        assert self.survivor.space_remaining == INITIAL_EQUIPMENT_LIMIT
 
     def test_setter(self):
         self.survivor.actions_remaining = 5
         assert self.survivor.actions_remaining == 5
+    
+    def test_pick_up_equipment(self):
+        expected_space_remaining = self.survivor.space_remaining - 1
+        self.survivor.pick_up('baseball bat')
+        assert self.survivor.space_remaining == expected_space_remaining
