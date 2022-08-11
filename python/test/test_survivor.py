bca8dee5577217c6b9fa4e60b95b79f4a235817b
Kyle Adams <kadams54@users.noreply.github.com>
2022-08-11 11:07:16 -0400

    Naming tweaks

--- python/test/test_survivor.py
+++ python/test/test_survivor.py
@@ -1,4 +1,4 @@
-from zombie_survivor.survivor import Survivor, INITIAL_EQUIPMENT_LIMIT
+from zombie_survivor.survivor import Survivor, EQUIPMENT_LIMIT
 
 
 class TestSurvivor:
@@ -12,18 +12,18 @@ class TestSurvivor:
         assert self.survivor.wound_count == 0
         assert self.survivor.is_alive()
         assert self.survivor.actions_remaining == 3
-        assert self.survivor.space_remaining == INITIAL_EQUIPMENT_LIMIT
+        assert self.survivor.space_remaining == EQUIPMENT_LIMIT
 
     def test_setter(self):
         self.survivor.actions_remaining = 5
         assert self.survivor.actions_remaining == 5
     
-    def test_pick_up_equipment(self):
+    def test_can_pick_up_equipment(self):
         expected_space_remaining = self.survivor.space_remaining - 1
         self.survivor.pick_up('baseball bat')
         assert self.survivor.space_remaining == expected_space_remaining
     
-    def test_survivor_can_be_wounded(self):
+    def test_can_be_wounded(self):
         expected_wound_count = self.survivor.wound_count + 1
         self.survivor.wound()
         assert self.survivor.wound_count == expected_wound_count
