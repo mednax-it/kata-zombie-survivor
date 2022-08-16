aae9286957d3973a32ee61312f31399d5bccb338
Kyle Adams <kadams54@users.noreply.github.com>
2022-08-16 10:59:50 -0400

    Allow adding survivors to a game
    Each survivor's name must be unique.

--- python/test/test_game.py
+++ python/test/test_game.py
@@ -1,6 +1,23 @@
-from zombie_survivor.game import Game
+import pytest
+
+from zombie_survivor.survivor import Survivor
+from zombie_survivor.game import DuplicateNameError, Game
 
 
 def test_game_started_with_no_survivors():
     game = Game()
     assert len(game.survivors) == 0
+
+
+def test_game_can_add_survivors():
+    game = Game()
+    expected_survivor_count = len(game.survivors) + 1
+    game.add_survivor(Survivor("Dread Pirate Roberts"))
+    assert len(game.survivors) == expected_survivor_count
+
+
+def test_game_can_not_add_survivors_with_same_name():
+    game = Game()
+    game.add_survivor(Survivor("Dread Pirate Roberts"))
+    with pytest.raises(DuplicateNameError):
+        game.add_survivor(Survivor("Dread Pirate Roberts"))
--- python/zombie_survivor/game.py
+++ python/zombie_survivor/game.py
@@ -1,3 +1,16 @@
+from zombie_survivor.survivor import Survivor
+
+
+class DuplicateNameError(Exception):
+    pass
+
+
 class Game:
     def __init__(self):
         self.survivors = []
+
+    def add_survivor(self, survivor: Survivor):
+        existing_names = map(lambda x: x.name, self.survivors)
+        if survivor.name in existing_names:
+            raise DuplicateNameError
+        self.survivors.append(survivor)
