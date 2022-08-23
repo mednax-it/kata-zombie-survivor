fc12cbd329a6f2ca0431fe3c6d08b4057aff1acd
Kyle Adams <kadams54@users.noreply.github.com>
2022-08-23 11:13:39 -0400

    WIP Implement ending a game when all survivors dead

--- python/zombie_survivor/game.py
+++ python/zombie_survivor/game.py
@@ -7,6 +7,10 @@ class DuplicateNameError(Exception):
     pass
 
 
+class SurvivorNotFoundError(Exception):
+    pass
+
+
 class Game:
     def __init__(self):
         self._survivors = []
@@ -19,3 +23,11 @@ class Game:
         if survivor.name in [s.name for s in self._survivors]:
             raise DuplicateNameError
         self._survivors.append(survivor)
+
+    def kill_survivor(self, survivor: Survivor):
+        if survivor not in self._survivors:
+            raise SurvivorNotFoundError
+        self._survivors.remove(survivor)
+
+    def is_finished(self) -> bool:
+        return len(self._survivors) == 0
