5b9583090e356b0ff9f8f888e90daeca6d28a974
Kyle Adams <kadams54@users.noreply.github.com>
2022-08-23 21:59:16 -0400

    Fix lint and formatting

--- python/test/test_zombie_survivor.py
+++ python/test/test_zombie_survivor.py
@@ -1,11 +1,14 @@
 from zombie_survivor.survivor import Survivor
 
+
 class TestSurvivor:
+    survivor: Survivor
+
     def setup(self):
-        self.survivor = Survivor('Gandalf')
+        self.survivor = Survivor("Gandalf")
 
     def test_creating_a_new_survivor(self):
-        assert self.survivor.name == 'Gandalf'
+        assert self.survivor.name == "Gandalf"
         assert self.survivor.wounds == 0
         assert self.survivor.remaining_actions == 3
         assert self.survivor.is_alive
