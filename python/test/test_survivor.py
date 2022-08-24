5087f1215f6d4778fb2993e7fb818b85d59329bb
Kyle Adams <kadams54@users.noreply.github.com>
2022-08-24 09:16:50 -0400

    Fix lint errors and formatting

--- python/test/test_survivor.py
+++ python/test/test_survivor.py
@@ -19,23 +19,23 @@ class TestSurvivor:
     def test_setter(self):
         self.survivor.actions_remaining = 5
         assert self.survivor.actions_remaining == 5
-    
+
     def test_can_pick_up_equipment(self):
         expected_space_remaining = self.survivor.space_remaining - 1
-        self.survivor.pick_up('baseball bat')
+        self.survivor.pick_up("baseball bat")
         assert self.survivor.space_remaining == expected_space_remaining
-    
+
     def test_can_not_pick_up_too_much_equipment(self):
         for _ in range(EQUIPMENT_LIMIT):
-            self.survivor.pick_up('baseball bat')
+            self.survivor.pick_up("baseball bat")
         with pytest.raises(NoSpaceRemainingError):
-            self.survivor.pick_up('baseball bat')
-    
+            self.survivor.pick_up("baseball bat")
+
     def test_can_be_wounded(self):
         expected_wound_count = self.survivor.wound_count + 1
         self.survivor.wound()
         assert self.survivor.wound_count == expected_wound_count
-    
+
     def test_wounds_decrease_equipment_space(self):
         expected_space_remaining = self.survivor.space_remaining - 1
         self.survivor.wound()
