3031462a557a2d4b9249b8ac08b960754864de11
Kyle Adams <kadams54@users.noreply.github.com>
2022-08-23 22:01:46 -0400

    Fix lint errors and formatting

--- python/test/test_zombie_survivor.py
+++ python/test/test_zombie_survivor.py
@@ -1,15 +1,18 @@
 from zombie_survivor.survivor import Survivor
 
-class TestSurvivor:
-  def setup(self):
-    self.survivor = Survivor('Fred')
-  
-  def test_initial_setup(self):
-    assert self.survivor.name == 'Fred'
-    assert self.survivor.wounds == 0
-    assert self.survivor.is_alive() == True
-    assert self.survivor.actions_remaining == 3
 
-  def test_does_survivor_die_after_2_wounds(self):
-    self.survivor.wounds = 2
-    assert not self.survivor.is_alive()
+class TestSurvivor:
+    survivor: Survivor
+
+    def setup(self):
+        self.survivor = Survivor("Fred")
+
+    def test_initial_setup(self):
+        assert self.survivor.name == "Fred"
+        assert self.survivor.wounds == 0
+        assert self.survivor.is_alive()
+        assert self.survivor.actions_remaining == 3
+
+    def test_does_survivor_die_after_2_wounds(self):
+        self.survivor.wounds = 2
+        assert not self.survivor.is_alive()
--- python/zombie_survivor/survivor.py
+++ python/zombie_survivor/survivor.py
@@ -1,8 +1,8 @@
 class Survivor:
-  def __init__(self, name):
-    self.name = name
-    self.wounds = 0
-    self.actions_remaining = 3
+    def __init__(self, name):
+        self.name = name
+        self.wounds = 0
+        self.actions_remaining = 3
 
-  def is_alive(self):
-    return self.wounds < 2
+    def is_alive(self):
+        return self.wounds < 2
