51fa6e6f787528bad76d3b5a61f2ab2901daa4fe
Kyle Adams <kadams54@users.noreply.github.com>
2022-08-23 22:06:08 -0400

    Fix lint errors and formatting

--- python/test/test_zombie_survivor.py
+++ python/test/test_zombie_survivor.py
@@ -1,11 +1,14 @@
 from zombie_survivor.survivor import Survivor
 
+
 class TestSurvivor:
-  def setup(self):
-    self.survivor = Survivor('Rob Zombie')
-  
-  def test_initial_setup(self):
-    assert self.survivor.name == 'Rob Zombie'
-    assert self.survivor.wounds == 0
-    assert self.survivor.is_alive() == True
-    assert self.survivor.actions_remaining() == 3
+    survivor: Survivor
+
+    def setup(self):
+        self.survivor = Survivor("Rob Zombie")
+
+    def test_initial_setup(self):
+        assert self.survivor.name == "Rob Zombie"
+        assert self.survivor.wounds == 0
+        assert self.survivor.is_alive()
+        assert self.survivor.actions_remaining() == 3
--- python/zombie_survivor/survivor.py
+++ python/zombie_survivor/survivor.py
@@ -1,13 +1,14 @@
 MAX_ACTIONS = 3
 
-class Survivor:
-  def __init__(self, name: str):
-    self.name = name
-    self.wounds = 0
-    self.actions = 0
 
-  def is_alive(self) -> bool:
-    return self.wounds < 2
-  
-  def actions_remaining(self) -> int:
-    return MAX_ACTIONS - self.actions
+class Survivor:
+    def __init__(self, name: str):
+        self.name = name
+        self.wounds = 0
+        self.actions = 0
+
+    def is_alive(self) -> bool:
+        return self.wounds < 2
+
+    def actions_remaining(self) -> int:
+        return MAX_ACTIONS - self.actions
