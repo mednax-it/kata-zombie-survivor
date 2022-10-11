7f390729199fc41507df784e97730f2844f5b16a
Kyle Adams <kadams54@users.noreply.github.com>
2022-10-11 10:54:32 -0400

    Switch to the staticmethod approach

--- python/zombie_survivor/history.py
+++ python/zombie_survivor/history.py
@@ -20,39 +20,33 @@ class History:
 history = History()
 
 
-@decorator
-def game_started(func, *args, **kwargs):
-    func(*args, **kwargs)
-    history.push(f"The game begins at: {datetime.utcnow()}")
-
-
-@decorator
-def survivor_added(func, *args, **kwargs):
-    func(*args, **kwargs)
-    [_, survivor] = args
-    history.push(f"The game adds a survivor: {survivor.name}")
-
-
-@decorator
-def item_picked_up(func, *args, **kwargs):
-    func(*args, **kwargs)
-    [survivor, item] = args
-    history.push(f"The survivor {survivor.name} picked up: {item}")
-
-
-@decorator
-def wounded(func, *args, **kwargs):
-    func(*args, **kwargs)
-    [survivor] = args
-    history.push(f"The survivor {survivor.name} is wounded")
-
-
 class Historian:
-    def __init__(self):
-        self.survivor_added = survivor_added
-        self.game_started = game_started
-        self.item_picked_up = item_picked_up
-        self.wounded = wounded
+    @staticmethod
+    @decorator
+    def game_started(func, *args, **kwargs):
+        func(*args, **kwargs)
+        history.push(f"The game begins at: {datetime.utcnow()}")
+
+    @staticmethod
+    @decorator
+    def survivor_added(func, *args, **kwargs):
+        func(*args, **kwargs)
+        [_, survivor] = args
+        history.push(f"The game adds a survivor: {survivor.name}")
+
+    @staticmethod
+    @decorator
+    def item_picked_up(func, *args, **kwargs):
+        func(*args, **kwargs)
+        [survivor, item] = args
+        history.push(f"The survivor {survivor.name} picked up: {item}")
+
+    @staticmethod
+    @decorator
+    def wounded(func, *args, **kwargs):
+        func(*args, **kwargs)
+        [survivor] = args
+        history.push(f"The survivor {survivor.name} is wounded")
 
 
 historian = Historian()
