54a4c1c772e2626e4763d76fa14d35da5ee71771
Kyle Adams <kadams54@users.noreply.github.com>
2022-10-11 10:55:51 -0400

    Test our decorator functions

--- python/test/test_history.py
+++ python/test/test_history.py
@@ -1,4 +1,6 @@
-from zombie_survivor.history import History
+from zombie_survivor.history import historian, History, history
+from zombie_survivor.survivor import Survivor
+from zombie_survivor.game import Game
 
 
 class TestHistory:
@@ -18,3 +20,60 @@ class TestHistory:
         assert self.history.pop() is None
         self.history.push(event)
         assert self.history.pop() == event
+
+
+class TestHistorian:
+    def test_game_started(self):
+        @historian.game_started
+        def test():
+            # Don't need to do anything
+            pass
+
+        test()
+
+        assert "game begins" in history.pop()
+
+    def test_survivor_added(self):
+        @historian.survivor_added
+        def test(game, survivor):
+            # Don't need to do anything
+            pass
+
+        survivor = Survivor(name="Fred")
+        game = Game()
+
+        test(game, survivor)
+
+        message = history.pop()
+        assert "adds a survivor" in message
+        assert survivor.name in message
+
+    def test_item_picked_up(self):
+        @historian.item_picked_up
+        def test(survivor, item):
+            # Don't need to do anything
+            pass
+
+        survivor = Survivor(name="Fred")
+        item = "baseball bat"
+
+        test(survivor, item)
+
+        message = history.pop()
+        assert "picked up" in message
+        assert survivor.name in message
+        assert item in message
+
+    def test_wounded(self):
+        @historian.wounded
+        def test(survivor):
+            # Don't need to do anything
+            pass
+
+        survivor = Survivor(name="Fred")
+
+        test(survivor)
+
+        message = history.pop()
+        assert "wounded" in message
+        assert survivor.name in message
